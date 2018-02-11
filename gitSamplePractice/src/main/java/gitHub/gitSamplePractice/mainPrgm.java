2da33ff24d304d2a8a013b2cb0fa50d596fb6999
Sahoo <sheela.sahoo@franklintempleton.com>
2018-02-11 14:13:23 +0530

    added all the commands for clone and pull in same sample3 class in src

--- gitSamplePractice/src/main/java/gitHub/gitSamplePractice/mainPrgm.java
+++ gitSamplePractice/src/main/java/gitHub/gitSamplePractice/mainPrgm.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 public class mainPrgm {
 
+	//clone the project using commant : git clone <github url of project location>
 	@Test
 	public void sample1() {
 		System.out.println("sample1");
--- gitSamplePractice/src/main/java/gitHub/gitSamplePractice/sample3.java
+++ gitSamplePractice/src/main/java/gitHub/gitSamplePractice/sample3.java
@@ -1,5 +1,7 @@
 package gitHub.gitSamplePractice;
 
+import org.testng.annotations.Test;
+
 public class sample3 {
 	
 	//steps for git to push the code to remote after modification 
@@ -9,6 +11,10 @@ public class sample3 {
 //	git commit -m "<comments>"
 //	git push
 	
+//commands to be followed when we want to save the copy to temparory and then again retrieve when required: git stash and git stash pop
+//commands to followed when we want updated project where others mi8 have commited from remote: git pull
+	
+	@Test
 	public void sampleprg3() {
 		System.out.println("sample3 program");
 	}
