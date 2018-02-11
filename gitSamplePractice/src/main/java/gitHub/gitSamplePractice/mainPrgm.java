de9ba234ed4d9ee0d5cd5ffb6faf951af49764d5
ssahoo1 <ssahoo1@HYDNBK136575.euro.corp.frk.com>
2018-02-11 13:28:23 +0530

    Git Practice commit

---
+++ gitSamplePractice/src/main/java/gitHub/gitSamplePractice/mainPrgm.java
@@ -0,0 +1,12 @@
+package gitHub.gitSamplePractice;
+
+import org.testng.annotations.Test;
+
+public class mainPrgm {
+
+	@Test
+	public void sample1() {
+		System.out.println("sample1");
+	}
+
+}
---
+++ gitSamplePractice/src/test/java/gitHub/gitSamplePractice/testPrgm.java
@@ -0,0 +1,11 @@
+package gitHub.gitSamplePractice;
+
+import org.testng.annotations.Test;
+
+public class testPrgm {
+	
+	@Test
+	public void sample2() {
+		System.out.println("sample2");
+	}
+}
