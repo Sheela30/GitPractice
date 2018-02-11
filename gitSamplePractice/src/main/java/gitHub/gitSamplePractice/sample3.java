e7ec665d01c08bbd0cd956868ad4d52aefd1e285
Sahoo <sheela.sahoo@franklintempleton.com>
2018-02-11 13:56:45 +0530

    added the steps to be followed for pushing the code to remote after modifications under comments section in sample3 class under src folder

--- gitSamplePractice/src/main/java/gitHub/gitSamplePractice/sample3.java
+++ gitSamplePractice/src/main/java/gitHub/gitSamplePractice/sample3.java
@@ -2,6 +2,13 @@ package gitHub.gitSamplePractice;
 
 public class sample3 {
 	
+	//steps for git to push the code to remote after modification 
+//	git status
+//	git add <filename displayed in console>
+//	git add .
+//	git commit -m "<comments>"
+//	git push
+	
 	public void sampleprg3() {
 		System.out.println("sample3 program");
 	}
